86289e5ec4352fb014e1eeec1cade0a5db966fd0
ihtishaamulhaq <66353905+ihtishaamulhaq@users.noreply.github.com>
2023-06-01 19:46:38 +0500

    SVM_iris.py

--- SVM_iris.py
+++ SVM_iris.py
@@ -1,9 +1,6 @@
 
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn import datasets, svm
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.neighbors import KNeighborsClassifier
-from xgboost import XGBClassifier
 
 # load the dataset from sklearn libraray
 J, k = datasets.load_iris(return_X_y=True)
@@ -14,8 +11,6 @@ J.shape, k.shape
 # use the train_test_split function to divide the data into taraining and test data. 
 J_train, J_test, k_train, k_test = train_test_split(
     J, k, test_size=0.2, random_state=0)
-
-J_train.shape, k_train.shape
 
 print(J_test.shape, k_test.shape)
 
