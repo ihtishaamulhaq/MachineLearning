727c34fc377fa4bc14b7b2e0f8f080a219230540
ihtishaamulhaq <66353905+ihtishaamulhaq@users.noreply.github.com>
2023-06-10 17:00:32 +0500

    SVM_iris.py

--- SVM_iris.py
+++ SVM_iris.py
@@ -19,7 +19,18 @@ SVM.fit(J_train, k_train)
 
 # printing the score
 print("SVM with linear kernel results= ",SVM.score(J_test, k_test))
+prediction=SVM.predict(J_test)
 
 # Cross Validation Applied on iris Dataset 
 scores = cross_val_score(SVM, J, k, cv=5)
 print("Result using Cross Validation = ",scores)
+
+# calculate the confusion matrix
+conf_metrics=metrics.confusion_matrix(k_test, Prediction)
+
+# draw the confusion matrix
+sns.heatmap(conf_metrics, annot=True)
+plt.ylabel("Predicted", fontsize=16)
+plt.xlabel("Actual", fontsize=16)
+plt.title("Confusion Matrix for Digit Dataset",fontsize=18)
+plt.show()
