7f89c2d2d4b55a11ceadad17e9a73b50db6f12b7
ihtishaamulhaq <66353905+ihtishaamulhaq@users.noreply.github.com>
2023-09-23 20:22:16 +0500

    Digit_Dataset.py

--- Digit_Classification/Digit_Dataset.py
+++ Digit_Classification/Digit_Dataset.py
@@ -1,5 +1,12 @@
 # In this program we use the random forest classifier for digit classification.
 
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 24 18:12:06 2023
+
+@author: Ihtishaam
+"""
+
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
@@ -9,6 +16,10 @@ import matplotlib.pyplot as plt
 
 # load the dataset from Sklearn (datasets library)
 A, b=datasets.load_digits(return_X_y=True)
+print(A.shape)
+print(b.shape)
+
+
 
 # Split the dataset into training and testing 
 I_train, I_test, J_train, J_test=train_test_split(A, b, test_size=0.3) 
@@ -23,12 +34,13 @@ Prediction=R_F_Classifier.predict(I_test)
 # calculate the confusion matrix
 conf_mat=metrics.confusion_matrix(J_test, Prediction)
 
-# draw the confusion matrix
-sns.heatmap(conf_mat, annot=True)
-plt.ylabel("Prediction")
-plt.xlabel("Actual")
-plt.title("Confusion Matrix for Digit Dataset",fontsize=16)
-plt.show()
+# Display the confusion matrix
+sns.heatmap(conf_mat, annot=True, fmt='d', cmap='plasma', cbar=False)
+plt.xlabel("actual")
+plt.ylabel("prediction")
+plt.title("Confusion Matrix on Digit Dataset",fontsize=16)
+# plt.show()
+plt.savefig("confusion Matrix.png")
 
 # compute the metrics like Accuracy, Precision, Recall, F1-score
 score=metrics.accuracy_score(J_test, Prediction)
@@ -39,3 +51,4 @@ score2=metrics.precision_score(J_test, Prediction, average='macro')
 print("Precision :",round(score2*100,2), "%")
 score3=metrics.f1_score(J_test, Prediction, average='macro')
 print("F1-Score  :",round(score3*100,2), "%")
+
