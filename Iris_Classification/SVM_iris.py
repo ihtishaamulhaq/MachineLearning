9ef140d653b255ee06b48a10dfeb5e7250d5e539
ihtishaamulhaq <ihtshaamanees@gmail.com>
2023-10-25 23:09:14 +0500

    update the SVM_iris.py and compute the confusion matrix

--- Iris_Classification/SVM_iris.py
+++ Iris_Classification/SVM_iris.py
@@ -1,15 +1,30 @@
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn import datasets, svm
+from sklearn.metrics import accuracy_score, f1_score, confusion_matrix
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 # load the dataset from sklearn libraray
-J, k = datasets.load_iris(return_X_y=True)
+dataset= datasets.load_iris(as_frame=True)
 
-# finding the shape of dataset
-J.shape, k.shape
+# finding the column names or feature names
+column_name=dataset.feature_names
+print(column_name)
+
+# Show the first five and last five rows
+features=dataset.data
+print(features)
+
+# Show the target values
+labels=dataset.target
+print(labels)
+
+# show the target names
+print(dataset.target_names)
 
 # use the train_test_split function to divide the data into taraining and test data. 
 J_train, J_test, k_train, k_test = train_test_split(
-    J, k, test_size=0.2, random_state=0)
+    features, labels, test_size=0.3, random_state=0)
 
 print(J_test.shape, k_test.shape)
 
@@ -19,18 +34,23 @@ SVM.fit(J_train, k_train)
 
 # printing the score
 print("SVM with linear kernel results= ",SVM.score(J_test, k_test))
-prediction=SVM.predict(J_test)
+Prediction=SVM.predict(J_test)
 
 # Cross Validation Applied on iris Dataset 
-scores = cross_val_score(SVM, J, k, cv=5)
+scores = cross_val_score(SVM, features, labels, cv=5)
 print("Result using Cross Validation = ",scores)
 
+# calculate the accuracy
+ACC=accuracy_score(k_test, Prediction)
+print("\n","Accuracy is = ",round(ACC*100,2),"%")
+
 # calculate the confusion matrix
-conf_metrics=metrics.confusion_matrix(k_test, Prediction)
+conf_metrics=confusion_matrix(k_test, Prediction)
 
 # draw the confusion matrix
-sns.heatmap(conf_metrics, annot=True)
+sns.heatmap(conf_metrics, annot=True, fmt='d', cbar=False)
 plt.ylabel("Predicted", fontsize=16)
 plt.xlabel("Actual", fontsize=16)
 plt.title("Confusion Matrix for Digit Dataset",fontsize=18)
+plt.savefig("confusion_matrix.png")
 plt.show()
